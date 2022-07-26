ab3e05a5c79fffcad681fcae9adc61faf4f1a487
김진호 <wlsghsla94@naver.com>
2022-07-26 21:45:02 +0900

    2027.대각선출력하기추가

---
+++ D1/2027.대각선출력하기.py
@@ -0,0 +1,27 @@
+"""
+    문제
+    
+    주어진 텍스트를 그대로 출력하세요.
+    
+    출력
+    #++++
+    +#+++
+    ++#++
+    +++#+
+    ++++#
+"""
+for i in range(1, 6):
+    for j in range(1, 6):
+        if i != j:
+            print('+', end = '')
+        else:
+            print('#', end = '')
+    print()
+    
+# 스터디그룹의 김수형님 답안
+s = ['+'] * 5
+ 
+for i in range(5):
+    s[i] = '#'
+    print(''.join(s))
+    s[i] = '+'
